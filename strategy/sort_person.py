263b94ee1621e005b63e8879f9df5da29a117e52
kinglion <1609019405@qq.com>
2021-06-07 21:48:12 +0800

    sort person

--- strategy/sort_person.py
+++ strategy/sort_person.py
@@ -19,19 +19,34 @@ class CompareByAge(ICompare):
         return person1.age - person2.age
 
 
+class CompareByWeight(ICompare):
+    def comparable(self, person1, person2):
+        return person1.weight - person2.weight
+
+
+class SortPerson:
+    def __init__(self, compare):
+        self.compare = compare
+
+    def sort(self, person_list):
+        n = len(person_list)
+        for i in range(0, n-1):
+            if self.compare.comparable(person_list[i], person_list[i+1]) > 0:
+                tmp = personList[i]
+                personList[i] = personList[i + 1]
+                personList[i + 1] = tmp
+            i += 1
+
+
 personList = [
     Person('Tom', 22, 70, 180),
     Person('Bob', 18, 42, 166),
     Person('Jenny', 32, 52, 175)
 ]
 
-n = len(personList)
-for i in range(0, n-1):
-    if CompareByAge().comparable(personList[i], personList[i+1]) > 0:
-        tmp = personList[i]
-        personList[i] = personList[i+1]
-        personList[i+1] = tmp
-    i += 1
+
+SortPerson(CompareByAge()).sort(personList)
+
 
 for p in personList:
     print(p)
