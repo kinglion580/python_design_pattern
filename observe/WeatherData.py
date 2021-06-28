4d0d93b4cd94dcc67ca37b0c91819e95b9eddedf
kinglion <1609019405@qq.com>
2021-06-29 00:51:12 +0800

    add heatindex

--- observe/WeatherData.py
+++ observe/WeatherData.py
@@ -116,13 +116,51 @@ class ForecastDisplay(Observer, DisplayElement):
             print("Watch out for cooler, rainy weather")
 
 
+class HeatIndexDisplay(Observer, DisplayElement):
+    heat_index = 0.0
+
+    def __init__(self, weather_data):
+        self.weather_data = weather_data
+        weather_data.register_observer(self)
+
+    def update(self, temp, humidity, pressure):
+        self.heat_index = ((16.923 + (0.185212 * temp) + (5.37941 * humidity) - (0.100254 * temp * humidity)
+                            + (0.00941695 * (temp * temp)) + (0.00728898 * (humidity * humidity))
+                            + (0.000345372 * (temp * temp * humidity)) - (0.000814971 * (temp * humidity * humidity))
+                            + (0.0000102102 * (temp * temp * humidity * humidity))
+                            - (0.000038646 * (temp * temp * temp)) + (0.0000291583 * (humidity * humidity * humidity))
+                            + (0.00000142721 * (temp * temp * temp * humidity))
+                            + (0.000000197483 * (temp * humidity * humidity * humidity))
+                            - (0.0000000218429 * (temp * temp * temp * humidity * humidity))
+                            + 0.000000000843296 * (temp * temp * humidity * humidity * humidity))
+                           - (0.0000000000481975 * (temp * temp * temp * humidity * humidity * humidity)))
+
+        self.display()
+
+    def display(self):
+        print("heat index is " + str(self.heat_index))
+
+
 class WeatherStation:
     weather_data = WeatherData()
     current_display = CurrentConditionsDisplay(weather_data)
     statistics_display = StatisticsDisplay(weather_data)
     forecast_display = ForecastDisplay(weather_data)
+    heat_index_display = HeatIndexDisplay(weather_data)
 
     weather_data.set_measurements(80, 65, 30.4)
     weather_data.set_measurements(82, 70, 29.2)
     weather_data.set_measurements(78, 90, 29.2)
+
+
+if __name__ == '__main__':
+    weatherdata = WeatherData()
+    current_display = CurrentConditionsDisplay(weatherdata)
+    statistics_display = StatisticsDisplay(weatherdata)
+    forecast_display = ForecastDisplay(weatherdata)
+    heat_index_display = HeatIndexDisplay(weatherdata)
+
+    weatherdata.set_measurements(80, 65, 30.4)
+    weatherdata.set_measurements(82, 70, 29.2)
+    weatherdata.set_measurements(78, 90, 29.2)
 
