f770eaf1a3c813dfc068ed38ba2276e99504274c
kinglion <1609019405@qq.com>
2021-07-04 19:31:48 +0800

    .2f

--- decorator/Baverage.py
+++ decorator/Baverage.py
@@ -80,19 +80,19 @@ class Whip(CondimentDecorator):
 
 if __name__ == '__main__':
     beverage = Espresso()
-    print(beverage.get_description() + " $%f" % beverage.cost())
+    print(beverage.get_description() + " $%.2f" % beverage.cost())
 
     beverage2 = DarkRoast()
     beverage2 = Mocha(beverage2)
     beverage2 = Mocha(beverage2)
     beverage2 = Whip(beverage2)
-    print(beverage2.get_description() + " $%f" % beverage2.cost())
+    print(beverage2.get_description() + " $%.2f" % beverage2.cost())
 
     beverage3 = HouseBlend()
     beverage3 = Soy(beverage3)
     beverage3 = Mocha(beverage3)
     beverage3 = Whip(beverage3)
-    print(beverage3.get_description() + " $%f" % beverage3.cost())
+    print(beverage3.get_description() + " $%.2f" % beverage3.cost())
 
 
 
