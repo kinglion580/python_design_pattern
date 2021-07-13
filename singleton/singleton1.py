f450edff05cb9913554c4da592a1a4f874397d10
yel2 <l.ye@dell.com>
2021-07-13 20:19:47 +0800

    singleton

--- singleton/singleton1.py
+++ singleton/singleton1.py
@@ -22,3 +22,10 @@ if __name__ == '__main__':
     print(tony.get_name(), karry.get_name())
     print(id(tony), "\n", id(karry))
     print(tony == karry)
+
+
+"""
+应用场景：
+1. 希望一个类只有一个且只能有一个实例
+2. 项目中的一些全局管理类 (Manager) 可以用单例模式来实现
+"""
